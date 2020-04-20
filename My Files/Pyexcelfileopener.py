8d159b7fb3acea807765f5ca2fbdf526c758dca2
douglas-davidi <59583281+douglas-davidi@users.noreply.github.com>
2020-04-20 12:37:25 -0400

    Additional Iterations and Class Objects
    Learned to add commanda at the beginning to understand how all the code works together vs jupiter  notebooks. Added and created the classs objects of products and reviews for this segment

--- My Files/Pyexcelfileopener.py
+++ My Files/Pyexcelfileopener.py
@@ -21,14 +21,78 @@ sheet["A1"].value
                            #values_only=True):
     #print(row)
 #testing for different values to see different results from sample data
-for value in sheet.iter_rows(min_row=2,
-                             min_col=4,
-                             max_col=7,
+#for value in sheet.iter_rows(min_row=2,
+                             #min_col=4,
+                             #max_col=7,
+                             #values_only=True):
+    #print(value)
+
+#for value in sheet.iter_rows(min_row=2,
+                            # min_col=4,
+                             #max_col=8,
+                             #values_only=True):
+    #print(value)
+
+# shows the names of the column headings
+#for value in sheet.iter_rows(min_row=1,
+                             #max_row=1,
+                             #values_only=True):
+    #print(value)
+ #shows values of selected columns 
+#for value in sheet.iter_rows(min_row=2,
+                             # min_col=4,
+                              #max_col=7,
+                              #values_only=True):
+    #print(value)
+import json
+from openpyxl import load_workbook
+
+workbook = load_workbook(filename="reviews-sample1.xlsx")
+sheet = workbook.active
+
+products = {}
+
+# Using the values_only because you want to return the cells' values
+for row in sheet.iter_rows(min_row=2,
+                           min_col=4,
+                           max_col=7,
+                           values_only=True):
+    product_id = row[0]
+    product = {
+        "parent": row[1],
+        "title": row[2],
+        "category": row[3]
+    }
+    products[product_id] = product
+
+# Using json here to be able to format the output for displaying later
+print(json.dumps(products))
+
+import datetime
+from dataclasses import dataclass
+
+@dataclass
+class Product:
+    id: str
+    parent: str
+    title: str
+    category: str
+
+@dataclass
+class Review:
+    id: str
+    customer_id: str
+    stars: int
+    headline: str
+    body: str
+    date: datetime.datetime
+
+for value in sheet.iter_rows(min_row=1,
+                             max_row=1,
                              values_only=True):
     print(value)
 
-for value in sheet.iter_rows(min_row=2,
-                             min_col=4,
-                             max_col=8,
-                             values_only=True):
-    print(value)
+
+# Or an alternative
+for cell in sheet[1]:
+    print(cell.value)
