6759ed168142aaf57e843024ef5068bb73891136
douglas-davidi <59583281+douglas-davidi@users.noreply.github.com>
2020-04-23 09:54:51 -0400

    First successful test creating a line chart
    This creates a line chart from raw excel data, based of actual data from employer(sanitized)

---
+++ My Files/mytestdata.py
@@ -0,0 +1,46 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.chart import BarChart, Reference
+from openpyxl.chart import LineChart, Reference
+
+    #Line 7  how to create a new empty workbook.
+    #Lines 13 & 14  how to add data to specific cells.
+    #Line 17 how to save the spreadsheet 
+
+filename = "My_Test_Data.xlsx"
+
+#workbook = Workbook()
+
+#sheet = workbook.active
+
+workbook = load_workbook(filename="My_Test_Data.xlsx")
+sheet = workbook.active
+
+#this is a bar chart
+# chart = BarChart()
+
+#data = Reference(worksheet=sheet,
+                # min_row=1,
+                 #max_row=8,
+                 #min_col=2,
+                 #max_col=3)
+
+#chart.add_data(data, titles_from_data=True)
+
+#sheet.add_chart(chart, "E2")
+chart = LineChart()
+data = Reference(worksheet=sheet,
+                 min_row=2,
+                 max_row=22,
+                 min_col=1,
+                 max_col=70)
+chart.add_data(data, from_rows=True, titles_from_data=True)
+
+sheet.add_chart(chart, "C6")
+chart.x_axis.title = "Weeks"
+chart.y_axis.title = "Count"
+
+
+workbook.save("line_chart.xlsx")
+
+workbook.save("chart.xlsx")
