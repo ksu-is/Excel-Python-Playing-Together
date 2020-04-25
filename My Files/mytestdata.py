25640e61c34e68b06f0eb3067305663df05e3484
douglas-davidi <59583281+douglas-davidi@users.noreply.github.com>
2020-04-25 19:15:24 -0400

    Added Title, Legend
    Can't figure out how to move the legend to bottom. Title is set.

--- My Files/mytestdata.py
+++ My Files/mytestdata.py
@@ -2,7 +2,10 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.chart import BarChart, Reference
 from openpyxl.chart import LineChart, Reference
-
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+from openpyxl.chart import ScatterChart, Series, Reference
+from openpyxl.chart.layout import Layout, ManualLayout
+from openpyxl.chart.axis import DateAxis
     #Line 7  how to create a new empty workbook.
     #Lines 13 & 14  how to add data to specific cells.
     #Line 17 how to save the spreadsheet 
@@ -39,8 +42,12 @@ chart.add_data(data, from_rows=True, titles_from_data=True)
 sheet.add_chart(chart, "C6")
 chart.x_axis.title = "Weeks"
 chart.y_axis.title = "Count"
-
-
+chart.title= " Count per Aircraft "
+chart.y_axis.title = "Count"
+#legend.position = "l"
+fill = PatternFill(fill_type=None,
+                start_color='FFFFFFFF',
+                end_color='FF000000')
 workbook.save("line_chart.xlsx")
 
 workbook.save("barchart.xlsx")
