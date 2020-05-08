2abf3c5ab7e9a5aa553eb34fd524eb53519797a6
douglas-davidi <59583281+douglas-davidi@users.noreply.github.com>
2020-05-08 16:27:28 -0400

    Automate the Boring Stuff Learning Program
    THis is my file folder for my Automate the Boring Stuff course on Udemy.com

---
+++ Automate Boring/Guess Number Game.py
@@ -0,0 +1,23 @@
+# this is a guess the number game
+
+import random
+print('Hello. What is your name?')
+name=input()
+secretNumber = random.randint(1,20)
+print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
+
+#Ask the player to guess 6 times.
+for guessesTaken in range (1,7):
+    print('Take a guess.')
+    guess=int(input())
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high')
+    else:
+        break # this condition is the correct answer!
+
+if guess == secretNumber:
+        print('Good Job, ' + name + '! You guess my number in ' + str(guessesTaken) + ' guesses')
+else:
+    print('Nope. The number I was thinking of was ' + str(secretNumber))
---
+++ Automate Boring/example.py
@@ -0,0 +1,8 @@
+def hello(name):
+    print('Howdy! ' + name)
+    print('Howdy!!!!')
+    print('Hello There!')
+hello('Alice')
+hello('Bob')
+len('Hello')
+#print('Hello has ' + str(len)('hello') + ' letters in it')
